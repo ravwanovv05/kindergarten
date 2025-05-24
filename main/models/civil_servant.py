ee594d9e8d32bbdb60af6f8bf18ca0d517cfb9a9
Di1shodbek02 <ahmatovdilshodbek@gmail.com>
2025-05-24 18:37:34 +0500

    delete civil servant model add to role

--- main/models/civil_servant.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class CivilServantModela(models.Model):
-    name = models.CharField(verbose_name='Name', max_length=100)
-
-    def __str__(self):
-        return self.name
